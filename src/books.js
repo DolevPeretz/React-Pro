0c8d15347860726da6af95be82d5452c6160be82
DolevPeretz <dolevop@gmail.com>
2025-06-03 10:52:26 +0300

    in

---
+++ src/books.js
@@ -0,0 +1,19 @@
+import book1 from "./Images/book1.jpg";
+import book2 from "./Images/book2.jpg";
+const books = [
+  {
+    author: "Jorden Moore",
+    alt: "It titile",
+    title: "Intersting Facts for curious Minds",
+    img: book1,
+    id: 1,
+  },
+  {
+    author: "Jorden Moore",
+    alt: "It titile",
+    title: "Intersting Facts for curious Minds",
+    img: book2,
+    id: 2,
+  },
+];
+export default books;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,20 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import book1 from "./Images/book1.jpg";
+import books from "./books";
+import Book from "./Book";
 
 function BookList() {
   return (
-    <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-    </section>
+    <>
+      <h1>Amazon best sellers</h1>
+      <section className="booklist">
+        {books.map((book, index) => {
+          return <Book {...book} key={book.id} number={index} />;
+        })}
+      </section>
+    </>
   );
 }
 
-const Book = () => {
-  return (
-    <article className="book">
-      <Image />
-      <Title />
-      <Author />
-    </article>
-  );
-};
-const Image = () => (
-  <img src={book1} alt="Intersting Facts for curious Minds" />
-);
-
-const Title = () => {
-  return <h2> My First Component </h2>;
-};
-const Author = () => <h4>Imge Placeholder</h4>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<BookList />);
